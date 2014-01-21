5f11527720899467ad49f978b601d2e8ead2d81c
nabin369 <nabinrai369@gmail.com>
2014-01-21 17:55:54 +0000

    updated docking_station_spec file

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -12,6 +12,8 @@ class DockingStation
 	end
 
 	def dock(bike)
+		# if the capacity is reached, raise an exception
+		raise "Station is full" if full?
 		@bikes << bike
 	end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,9 @@ describe DockingStation do
 
 let(:bike) { Bike.new }
 let(:station) { DockingStation.new(:capacity => 20) }
+def fill_station(station)
+	20.times { station.dock(Bike.new) }
+end
 
 it "should accept a bike" do
 	#The station is expected to have 0 bikes.
@@ -22,9 +25,15 @@ end
 
 	it "should know when it's full" do
 		expect(station).not_to be_full
-		20.times { station.dock(Bike.new) }
+		fill_station(station)
 		expect(station).to be_full
 	end
+
+	it "should not accept a bike if its full" do
+		fill_station(station)
+		expect(lambda { station.dock(bike) }).to raise_error(RuntimeError)
+	end
+
 
 
 end
