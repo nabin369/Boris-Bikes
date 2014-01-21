8d1f39b0bb36276f1a38235c7272dfa64e8acf55
nabin369 <nabinrai369@gmail.com>
2014-01-21 19:41:48 +0000

    added bike_container_spec, and updated docking_station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,34 +1,17 @@
+# load BikeContainer
+require_relative 'bike_container'
+
 class DockingStation
 
-	DEFAULT_CAPACITY = 10
+	# this gives us all the methods that used to be in this class
+	include BikeContainer
 
-	def initialize(options = {})
-		@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
-		@bikes = []
+	def initialize(option = {})
+		# self.capacity is calling the capacity=() method
+		# (note the equals sign) defined in BikeContainer
+		# capacity (the seond argument to fetch()) is calling
+		# the capacity() method in BikeContainer
+		self.capacity = option.fetch(:capacity, capacity)
 	end
-
-	def bike_count
-		@bikes.count
-	end
-
-	def dock(bike)
-		# if the capacity is reached, raise an exception
-		raise "Station is full" if full?
-		@bikes << bike
-	end
-
-	def release(bike)
-		@bikes.delete(bike)
-	end
-
-	def full?
-		bike_count == @capacity
-	end
-
-	def available_bikes
-		@bikes.reject {|bike| bike.broken? }
-	end
-
-
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,45 +2,51 @@ require_relative "../lib/docking_station"
 
 describe DockingStation do
 
-let(:bike) { Bike.new }
-let(:station) { DockingStation.new(:capacity => 20) }
-def fill_station(station)
-	20.times { station.dock(Bike.new) }
-end
+	let(:station) { DockingStation.new(:capacity => 123)}
 
-it "should accept a bike" do
-	#The station is expected to have 0 bikes.
-	expect(station.bike_count).to eq(0)
-	#Docking a bike in the station
-	station.dock(bike)
-	#Now we expect it to have one bike
-	expect(station.bike_count).to eq(1)
-end
+	it "should allow setting default capacity on initialisig" do
+		expect(station.capacity).to eq(123)
+	end
+
+# let(:bike) { Bike.new }
+# let(:station) { DockingStation.new(:capacity => 20) }
+# def fill_station(station)
+# 	20.times { station.dock(Bike.new) }
+# end
+
+# it "should accept a bike" do
+# 	#The station is expected to have 0 bikes.
+# 	expect(station.bike_count).to eq(0)
+# 	#Docking a bike in the station
+# 	station.dock(bike)
+# 	#Now we expect it to have one bike
+# 	expect(station.bike_count).to eq(1)
+# end
 	
-	it "should release a bike" do
-		station.dock(bike)
-		station.release(bike)
-		expect(station.bike_count).to eq(0)
-	end
+# 	it "should release a bike" do
+# 		station.dock(bike)
+# 		station.release(bike)
+# 		expect(station.bike_count).to eq(0)
+# 	end
 
-	it "should know when it's full" do
-		expect(station).not_to be_full
-		fill_station(station)
-		expect(station).to be_full
-	end
+# 	it "should know when it's full" do
+# 		expect(station).not_to be_full
+# 		fill_station(station)
+# 		expect(station).to be_full
+# 	end
 
-	it "should not accept a bike if its full" do
-		fill_station(station)
-		expect(lambda { station.dock(bike) }).to raise_error(RuntimeError)
-	end
+# 	it "should not accept a bike if its full" do
+# 		fill_station(station)
+# 		expect(lambda { station.dock(bike) }).to raise_error(RuntimeError)
+# 	end
 
-	it "should provide a list of available bikes" do
-		working_bike, broken_bike = Bike.new, Bike.new
-		broken_bike.break
-		station.dock(working_bike)
-		station.dock(broken_bike)
-		expect(station.available_bikes).to eq([working_bike])
-	end
+# 	it "should provide a list of available bikes" do
+# 		working_bike, broken_bike = Bike.new, Bike.new
+# 		broken_bike.break
+# 		station.dock(working_bike)
+# 		station.dock(broken_bike)
+# 		expect(station.available_bikes).to eq([working_bike])
+# 	end
 
 
 end
